63ea5be28c59dd74d4c8a87993fc2825da38aeb1
vickykimani <victorianightingale70@gmail.com>
2022-03-16 16:18:47 +0300

    initial setup

---
+++ index.ts
@@ -0,0 +1,25 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+
+client.on('ready', () => {
+    console.log('the bot is ready')
+})
+
+client.on('messageCreate', (message) => {
+    if(message.content === 'ping') {
+        message.reply({
+            content: 'hey you <3',
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
